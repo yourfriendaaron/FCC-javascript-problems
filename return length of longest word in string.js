28431ea233a9fddd4f289a93cab283ea908707d6
Aaron Taylor <yourfriendaaron@users.noreply.github.com>
2015-10-21 23:28:42 -0400

    Update return length of longest word in string.js

--- return length of longest word in string.js
+++ return length of longest word in string.js
@@ -2,8 +2,8 @@ function findLongestWord(str) {
   str = str.split(" ").sort(function(a,b) {
     return b.length - a.length;
   });
-  console.log(str);
   return str.shift().length;
 }
 
-findLongestWord("The quick brown fox jumped over the lazy dog");
+findLongestWord("The quick brown fox jumped over the lazy dog"); // returns 6
+findLongestWord("1 222 333 999999999"); // returns 9
