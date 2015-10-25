f947d0e50c96c849aabe6ae5b6bdfcfa81a708f5
Aaron Taylor <yourfriendaaron@users.noreply.github.com>
2015-10-24 23:02:25 -0400

    Create biggest numbers in arrays.js

---
+++ biggest numbers in arrays.js
@@ -0,0 +1,13 @@
+// this function returns an array consisting of the largest number in each given array.
+// it takes as input any series of arrays of any lenth.
+
+function largestOfArray(arr) {
+  var arrNew = [];
+  for (var i=0; i < arr.length; i++) {
+    arrNew.push(arr[i].sort(function(a, b){return b-a})[0]);
+  };
+  return arrNew;
+}
+
+console.log(largestOfArrays([[-2.3, 0, 1.1], [70, 66, 67, 68, 69], [32, 35, 2067, 37, 39, 99], [12.1, 9, 0, -786], [-12, -142, -3.6, -0.015]]));
+// the above returns "[1.1, 70, 2067, 12.1, -0.015]"
