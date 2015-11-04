dbd26b07d17162ef57739518f603ac3940e7d0e0
Aaron Taylor <yourfriendaaron@users.noreply.github.com>
2015-11-03 22:27:00 -0500

    Create string mutation checker.js

---
+++ string mutation checker.js
@@ -0,0 +1,28 @@
+/*
+Return true if the string in the first element of the array
+contains all of the letters of the string in the second element of
+the array.
+*/
+
+function mutation(arr) {
+  arr = arr.map(function(x) {
+    return x.toLowerCase();
+  });
+  for (i = 0; i < arr[1].length; i++) {
+    if (arr[0].indexOf(arr[1].charAt(i)) >= 0) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(mutation(["hello", "hey"])); // returns false
+console.log(mutation(["hello", "Hello"])); // returns true
+console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"])); // returns true
+console.log(mutation(["Mary", "Army"])); // returns true
+console.log(mutation(["Mary", "Aarmy"])); // returns true
+console.log(mutation(["Alien", "line"])); // returns true
+console.log(mutation(["floor", "for"])); // returns true
+console.log(mutation(["hello", "neo"])); // returns false
